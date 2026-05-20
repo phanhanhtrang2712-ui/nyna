15f841ca222623d7941aa9e409ca3bcdd1fac697
phanhanhtrang2712-ui <phanhanhtrang2712@gmail.com>
2026-05-20 14:03:07 +0700

    Update Admin.tsx

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -121,7 +121,7 @@ const AdminPage = () => {
           </form>
           
           <div className="mt-8 pt-8 border-t border-gray-100 text-[10px] text-center text-gray-400 uppercase tracking-widest leading-relaxed">
-            Mặc định: admin / 123456123456
+            Quản trị: BINHPHAN
           </div>
         </div>
       </div>
